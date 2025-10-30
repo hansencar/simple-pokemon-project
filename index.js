d78b8c38f39ad54e04b41b2e91bf4d27641b0c0a
Hans <hansencar@hotmail.com>
2025-10-29 23:26:27 -0400

    display pokemonsprite on the website of the pokemone inputted into text field by the user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 //         data asynchronously over the web.
 //         fetch(url, {options})
 
+/*
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     .then(response => {
 
@@ -15,3 +16,29 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     })
     .then(data => console.log(data))
     .catch(error => console.error(error));
+*/
+
+async function fetchData(){
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+        
+        //console.log(data);
+    
+    }
+    catch(error){
+        console.error(error);
+    }
+}
