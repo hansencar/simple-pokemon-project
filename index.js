9584e0311e1589edaf59b214435dafc268c95d4f
Hans <hansencar@hotmail.com>
2025-10-29 22:52:06 -0400

    Add fetch function in index.js to call PokeAPI and create index.html

---
+++ index.js
@@ -0,0 +1,17 @@
+// fetch = Function used for making HTTP requests to fetch resources.
+//         (JSON style data, images, files)
+//         Simplifies asynchronous data fetching in JavaScript and
+//         used for interacting with APIs to retrieve and send
+//         data asynchronously over the web.
+//         fetch(url, {options})
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
